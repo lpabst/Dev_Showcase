9c21ac8800588a3e48aec94f2c34c052fd472204
loren pabst <lorenpabst@gmail.com>
2017-09-09 15:12:47 -0600

    routes are ready and nav is styled

--- src/components/NavHeader/NavHeader.js
+++ src/components/NavHeader/NavHeader.js
@@ -10,7 +10,11 @@ class NavHeader extends Component {
       <div className="nav_header">
 
         <ul className='nav_list'>
+            <Link to='/'>Home</Link>
             <Link to='/conditional-rendering'>Conditional Rendering</Link>
+            <Link to='/themes'>Themes</Link>
+            <Link to='/modals'>Modals</Link>
+            <Link to='/events'>Events</Link>
         </ul>
 
       </div>
