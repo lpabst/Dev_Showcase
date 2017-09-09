46e15342fa79b1c804b1d77f218c72b39e6a3ccf
loren pabst <lorenpabst@gmail.com>
2017-09-09 14:56:22 -0600

    added new routes

---
+++ src/components/ConditionalRendering/ConditionalRendering.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './ConditionalRendering.css';
+
+
+class ConditionalRendering extends Component {
+
+  render() {
+    return (
+      <div className="conditional_rendering">
+
+        Conditional Rendering
+
+      </div>
+    );
+  }
+}
+
+
+export default ConditionalRendering;
--- src/router.js
+++ src/router.js
@@ -3,22 +3,20 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Home from './components/Home/Home.js';
-// import Home from './components/Home/Home.js';
-// import Home from './components/Home/Home.js';
-// import Home from './components/Home/Home.js';
-// import Home from './components/Home/Home.js';
-// import Home from './components/Home/Home.js';
+import ConditionalRendering from './components/ConditionalRendering/ConditionalRendering.js';
+import Themes from './components/Themes/Themes.js';
+import Modals from './components/Modals/Modals.js';
+import Events from './components/Events/Events.js';
 
 
 export default (
     <Switch>
         
-        {/* <Route component={ Home } path='/' exact />
         <Route component={ Home } path='/' exact />
-        <Route component={ Home } path='/' exact />
-        <Route component={ Home } path='/' exact />
-        <Route component={ Home } path='/' exact /> */}
-        <Route component={ Home } path='/' exact />
+        <Route component={ ConditionalRendering } path='/conditional-rendering' exact />
+        <Route component={ Themes } path='/themes' exact />
+        <Route component={ Modals } path='/modals' exact />
+        <Route component={ Events } path='/events' exact /> 
 
     </Switch>
 )
