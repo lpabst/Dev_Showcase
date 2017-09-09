adadb7552f8e619160354e8c85b3539ce151cb18
loren pabst <lorenpabst@gmail.com>
2017-09-09 15:52:03 -0600

    nav header works great and is styled now

--- src/components/NavHeader/NavHeader.js
+++ src/components/NavHeader/NavHeader.js
@@ -4,12 +4,62 @@ import './NavHeader.css';
 
 
 class NavHeader extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      showMobileMenu: false
+    }
+
+    this.adjustMenuForScreenSize = this.adjustMenuForScreenSize.bind(this);
+    this.openMobileMenu = this.openMobileMenu.bind(this);
+    this.closeMobileMenu = this.closeMobileMenu.bind(this);
+  }
+
+  componentDidMount(){
+    this.adjustMenuForScreenSize();
+    window.addEventListener('resize', this.adjustMenuForScreenSize);
+  }
+
+  adjustMenuForScreenSize(){
+    let screenWidth = window.innerWidth;
+    if (screenWidth > 550){
+      this.openMobileMenu();
+    }else{
+      this.closeMobileMenu();
+    }
+  }
+
+  openMobileMenu(){
+    this.setState({
+      showMobileMenu: true
+    })
+  }
+
+  closeMobileMenu(){
+    this.setState({
+      showMobileMenu: false
+    })
+  }
 
   render() {
+
+    let hamStyle = null;
+    let menuStlye = null;
+    if (this.state.showMobileMenu){
+      hamStyle = {'top': '-120px'}
+      menuStlye = {'right': '0px'}
+    }else{
+      hamStyle = {'top': '15px'}
+      menuStlye = {'right': '-350px'}     
+    }
+
     return (
       <div className="nav_header">
 
-        <ul className='nav_list'>
+        <div className='ham' onClick={ this.openMobileMenu } style={hamStyle}>...</div>
+
+        <ul className='nav_list' style={menuStlye}>
+            <p onClick={ this.closeMobileMenu }>X</p>
             <Link to='/'>Home</Link>
             <Link to='/conditional-rendering'>Conditional Rendering</Link>
             <Link to='/themes'>Themes</Link>
